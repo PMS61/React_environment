306f30450efa7fbdbc4ea6d77918ca1dcab5a009
Prathamesh Sankhe <144032313+PMS61@users.noreply.github.com>
2024-03-03 15:09:20 +0530

    Files added

---
+++ src/components/search/search.jsx
@@ -0,0 +1,38 @@
+import { AsyncPaginate } from "react-select-async-paginate";
+import { useState } from "react";
+import { GEO_API_URL, geoApiOptions } from "../../api";
+const Search = ({ onSearchChange }) => {
+    const [search, setSearch] = useState(null);
+    const loadOptions = async (inputValue) => {
+        try {
+            const response = await fetch(`${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`, geoApiOptions);
+            const result = await response.json();
+            return {
+                options: result.data.map((city) => {
+                    return {
+                        value: `${city.latitude} ${city.longitude}`,
+                        label: `${city.name} ${city.countryCode}`,
+                    }
+                }),
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    const handleOnChange = (searchData) => {
+        setSearch(searchData);
+        onSearchChange(searchData)
+    }
+    return (
+        <AsyncPaginate
+            placeholder="Search for city"
+            debounceTimeout={600}
+            value={search}
+            onChange={handleOnChange}
+            loadOptions={loadOptions}
+        />
+    )
+}
+
+export default Search;
