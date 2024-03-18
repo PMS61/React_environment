74eb7fbb3e5e9745850bd61155d0ea620020e394
Prathamesh Sankhe <144032313+PMS61@users.noreply.github.com>
2024-03-18 14:46:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Search from './components/search/search';
 import CurrentWeather from './components/current-weather/current-weather';
 import Forecast from './components/forecast/forecast';
 import { useState } from 'react';
-import { WEATHER_API_URL } from './api';
 import { WEATHER_API_KEY } from './api';
+import { WEATHER_API_URL } from './api';
 
 function App() {
   const [currentWeather, setCurrentWeather] = useState(null);
